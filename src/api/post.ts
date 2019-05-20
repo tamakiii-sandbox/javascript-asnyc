3fd4480b31a759d8826810f66f7826f743f2925f
tamakiii <tamakiii@users.noreply.github.com>
2019-05-20 23:31:36 +0900

    impl post /posts

---
+++ src/api/post.ts
@@ -0,0 +1,42 @@
+export interface Post {
+    id?: number
+    title: string
+    author: string
+}
+
+export interface PostBody {
+    id: number
+    title: string
+    author: string
+}
+
+export interface ApiError {
+    message: string
+}
+
+export type ApiResult<Ok, Error> =
+    | {
+        status: true
+        body: PostBody
+    }
+    | {
+        status: false
+        body: ApiError
+    }
+
+export type PostResult = ApiResult<PostBody, ApiError>;
+
+const headers = {
+    "X-Requested-With": "XMLHttpRequest",
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+}
+
+export const post = (post: Post) => (
+    fetch('http://localhost:3000/api/posts', {
+        method: "POST",
+        credentials: "same-origin",
+        headers,
+        body: JSON.stringify(post),
+    })
+)
