8ae3852c2fbcecf5fe9b48b5511c42f8e8877395
tamakiii <tamakiii@users.noreply.github.com>
2019-05-20 23:30:53 +0900

    server.js returns index.js and dist/index.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
-// server.js
+const path = require('path')
+
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../index.html'))
+})
+server.get('/dist/index.js', (req, res) => {
+    res.sendFile(path.join(__dirname + '/../dist/index.js'))
+})
+
 server.use(middlewares)
-server.use(router)
+server.use('/api', router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
 })
